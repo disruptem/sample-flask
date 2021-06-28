2f04f7751966093869be9b5c94dd1ef4693b1513
khaled <khaled.nakawa@disruptem.com>
2021-06-27 23:59:18 -0400

    added appium and selenium

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 from flask import Flask
 from flask import render_template
+from appium import webdriver
+from appium.webdriver.common.mobileby import MobileBy
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
 
 app = Flask(__name__)
 
