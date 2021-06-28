73785088cd28087a06981224f15bb6229b5d8cf3
khaled <khaled.nakawa@disruptem.com>
2021-06-28 00:13:55 -0400

    added browserstack work part1

--- app.py
+++ app.py
@@ -9,6 +9,24 @@ import time
 app = Flask(__name__)
 
 
+userName = "khalednakawa_jskeyQ"
+accessKey = "gq16w425AZqW3SA5nPM2"
+
+desired_caps = {
+    "build": "browser_sample2",
+    "name" : "test 15",
+    "device": "iPhone 12 Pro",
+    "os_version" : "14",
+    "app": "bs://ac09441d0ad0f46bc4daa57af4553806ea6b8526",
+    # "browserstack.appium_version" : "1.21.0",
+    # "browserstack.acceptInsecureCerts" : "true",
+    # 'autoDissmissAlerts': 'true',
+     "autoAcceptAlerts" : "true",
+    # "autoGrantPermissions": "true",
+    # "gpsEnabled" : "true",
+}
+
+
 @app.route("/")
 def hello_world():
     print("testt")
